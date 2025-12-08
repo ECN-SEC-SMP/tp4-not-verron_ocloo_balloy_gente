76106f301fee33df9c35456b4dd83b76b80c0629
Aurelien Gente <a.gente1709@gmail.com>
2025-12-08 16:39:15 +0100

    Modifs commentaires

--- src/Parcelle.cpp
+++ src/Parcelle.cpp
@@ -1,28 +1,12 @@
-/**
- * @file Parcelle.cpp
- * @brief Implémentation de la classe Parcelle
- */
-
 #include "Parcelle.hpp"
 
 // Constructeurs
 
-/**
- * @brief Constructeur par défaut
- * 
- * Initialise le numéro à 0 et le propriétaire à une chaîne vide
- */
 Parcelle::Parcelle(){
     this->numero = 0;
     this->proprietaire = "";
 }
 
-/**
- * @brief Constructeur paramétré
- * @param num Numéro de la parcelle
- * @param prop Nom du propriétaire
- * @param forme Polygone représentant la forme de la parcelle
- */
 Parcelle::Parcelle(int num, std::string prop, Polygone<int> forme){
     this->numero = num;
     this->proprietaire = prop;
@@ -31,79 +15,46 @@ Parcelle::Parcelle(int num, std::string prop, Polygone<int> forme){
 
 // Getteurs
 
-/**
- * @brief Obtient le numéro de la parcelle
- * @return Le numéro de la parcelle
- */
 int Parcelle::getNumero() const{
     return numero;
 }
 
-/**
- * @brief Obtient le nom du propriétaire
- * @return Le nom du propriétaire de la parcelle
- */
 std::string Parcelle::getProprietaire() const{
     return proprietaire;
 }
 
-/**
- * @brief Obtient la surface de la parcelle
- * @return La surface de la parcelle en m²
- */
 float Parcelle::getSurface() const{
     return surface;
 }
 
-/**
- * @brief Obtient la forme géométrique de la parcelle
- * @return Le polygone représentant la forme de la parcelle
- */
 const Polygone<int>& Parcelle::getForme() const{
     return forme;
 }
 
-/**
- * @brief Obtient le pourcentage constructible
- * @return Le pourcentage constructible de la parcelle
- */
 int Parcelle::getPConstructible() const{
     return pConstructible;
 }
 
 // Setteurs
 
-/**
- * @brief Définit le numéro de la parcelle
- * @param n Le nouveau numéro de la parcelle
- */
 void Parcelle::setNumero(int n){
     this->numero = n;
 }
 
-/**
- * @brief Définit le propriétaire de la parcelle
- * @param prop Le nom du nouveau propriétaire
- */
 void Parcelle::setProprietaire(std::string prop){
     this->proprietaire = prop;
 }
 
-/**
- * @brief Définit la forme géométrique de la parcelle
- * @param forme Le polygone représentant la nouvelle forme
- */
 void Parcelle::setForme(Polygone<int> forme){
     this->forme = forme;
 }
 
-/**
- * @brief Définit le pourcentage constructible de la parcelle
- * @param p Le nouveau pourcentage constructible
- */
 void Parcelle::setPConstructible(int p){
     this->pConstructible = p;
 }
+
+// Surcharge de <<
+
 std::ostream& operator<<(std::ostream &o, Parcelle const& p)
 {
     o << "Parcelle n°" << p.numero << " :" << std::endl;
@@ -115,10 +66,8 @@ std::ostream& operator<<(std::ostream &o, Parcelle const& p)
     return o;
 }
 
-/**
- * @brief Calcule la surface de la parcelle en m²
- * @return La surface calculée
- */
+// Méthode
+
 float Parcelle::calculerSurface(){
     float area = 0.0;
     std::vector<Point2D<int>> sommets = forme.getSommets();
